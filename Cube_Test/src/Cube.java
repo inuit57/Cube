04d4cf594bd805cdb6bb93ff9e95c1a7448f15c7
narD <inuit57@naver.com>
2021-01-12 12:54:02 +0900

    Revert "https://lucas.codesquad.kr/main/course/2021%EB%85%84-%EB%A7%88%EC%8A%A4%ED%84%B0%EC%A6%88-%EC%98%A8%EB%9D%BC%EC%9D%B8%EB%AC%B8%EC%A0%9C/2021-problem-1/2%EB%8B%A8%EA%B3%84--%ED%8F%89%EB%A9%B4-%ED%81%90%EB%B9%85-%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0"
    This reverts commit bf162c5e19ad34fa675dd8b007a4f3276da28025.

--- Cube_Test/src/Cube.java
+++ Cube_Test/src/Cube.java
@@ -3,75 +3,9 @@ public class Cube {
 	
 	String str ; 
 	
-	char[][] cube = {{'R','R','W'},
-					 {'G','C','W'},
-					 {'G','B','B'}	} ; 
-	
 	public Cube(String str) {
 		this.str = str; 
 	}
-	
-	public void printCube() {
-		for(int i=0 ; i<3; i++) {
-			for(int j=0 ; j<3 ; j++) {
-				System.out.printf("%c ", cube[i][j]);
-			}
-			System.out.println("");
-		}
-	}
-	
-	public void cube_U(char ch) {
-		this.str = ""; 
-		for(int i= 0 ; i< 3; i++) {
-			str+= cube[0][i]; 
-		}
-		if (ch == 'U') {this.moveLeft();}
-		else { this.moveRight();} // ch == 'U'' 
-		
-		for(int i= 0 ; i<3; i++) {
-			cube[0][i] = this.str.charAt(i); 
-		}
-	}
-	
-	public void cube_R(char ch) {
-		this.str = "";
-		for(int i= 0 ; i<3; i++) {
-			str += cube[i][2] ; 
-		}
-		if (ch == 'R') {this.moveLeft();}
-		else { this.moveRight();}
-		
-		for(int i= 0 ; i<3; i++) {
-			cube[i][2] = this.str.charAt(i); 
-		}
-	}
-	
-	public void cube_L(char ch) {
-		this.str = "";
-		for(int i= 0 ; i<3; i++) {
-			str += cube[i][0] ; 
-		}
-		if (ch != 'L') {this.moveLeft();}
-		else { this.moveRight();}
-		
-		for(int i= 0 ; i<3; i++) {
-			cube[i][0] = this.str.charAt(i); 
-		}
-	}
-	
-	public void cube_D(char ch) {
-		this.str = "";
-		for(int i= 0 ; i<3; i++) {
-			str += cube[2][i] ; 
-		}
-		if (ch != 'D') {this.moveLeft();}
-		else { this.moveRight();}
-		
-		for(int i= 0 ; i<3; i++) {
-			cube[2][i] = this.str.charAt(i); 
-		}
-	}
-	
 	
 	public void moveLeft() {
 		char[] tmp = new char[this.str.length()]; 
--- Cube_Test/src/CubeMain.java
+++ Cube_Test/src/CubeMain.java
@@ -1,125 +1,21 @@
-import java.util.Scanner;
 
 public class CubeMain {
 	
-	private static Scanner sc;
-
 	public static void main(String[] args) {
 		
-		Cube c = new Cube("abcdefghijk");
-		String str;
-		sc = new Scanner(System.in);
-		boolean flag = true; 
+		Cube c = new Cube("abcdefghijk"); 
 		
+		c.moveLeft();
+		System.out.println(c.str);
 		
+		c.moveLeft(2); 
+		System.out.println(c.str);
 		
-		c.printCube();
+		c.moveRight(3);
+		System.out.println(c.str);
 		
-		while(flag) {
-			System.out.print("CUBE > ");
-			str = sc.nextLine(); 
-			
-			System.out.println();
-			
-			char ch_arr[] = new char[str.length()]; 
-			ch_arr = str.toCharArray(); 
-			
-			for(int i=0 ; i< ch_arr.length ; i++) {
-				if(ch_arr[i] == 'Q') {
-					System.out.println("Bye~");
-					//exit();
-					flag = false; 
-				}
-				
-				if ( ch_arr[0] == '\'') {
-					System.out.println("잘못된 입력감지! 다시 입력해주세요.");
-					break; 
-				}
-				
-				switch(ch_arr[i]) 
-				{
-	
-				case 'U' : 
-					System.out.print('U');
-					if ( (i+1 < ch_arr.length) &&  (ch_arr[i+1] =='\'') ) {
-						System.out.print('\'');
-						i++; 
-						c.cube_U('r'); // U'
-					}
-					else {
-						c.cube_U('U'); 
-					}
-					break;
-				case 'D' :
-					System.out.print('D');
-					if ( (i+1 < ch_arr.length) &&  (ch_arr[i+1] =='\'') ) {
-						System.out.print('\'');
-						i++; 
-						c.cube_D('r'); // U'
-					}
-					else {
-						c.cube_D('D'); 
-					}
-					break; 
-				case 'R' : 
-					System.out.print('R');
-					if ( (i+1 < ch_arr.length) &&  (ch_arr[i+1] =='\'') ) {
-						System.out.print('\'');
-						i++; 
-						c.cube_D('r'); // U'
-					}
-					else {
-						c.cube_D('R'); 
-					}
-					break;
-				case 'L' : 
-					System.out.print('L');
-					if ( (i+1 < ch_arr.length) &&  (ch_arr[i+1] =='\'') ) {
-						System.out.print('\'');
-						i++; 
-						c.cube_L('r'); // U'
-					}
-					else {
-						c.cube_L('L'); 
-					}
-					break; 
-				} // swith 문 끝. 
-				
-				if(flag) {
-					System.out.println();
-					c.printCube(); 
-					System.out.println();
-				}
-			} // for문 끝.
-		} //while(flag) 끝. 
-		
-				
-//		c.cube_U('U');
-//		c.printCube();
-		
-//		c.cube_D('D');
-//		c.printCube();
-		
-//		c.cube_L('L');
-//		c.printCube();
-		
-//		c.cube_R('R'); 
-//		c.printCube();
-		
-//		c.moveLeft();
-//		System.out.println(c.str);
-//		
-//		c.moveLeft(2); 
-//		System.out.println(c.str);
-//		
-//		c.moveRight(3);
-//		System.out.println(c.str);
-//		
-//		c.moveRight();
-//		System.out.println(c.str);
+		c.moveRight();
+		System.out.println(c.str);
 	}
 
 }
-	
-	
-
